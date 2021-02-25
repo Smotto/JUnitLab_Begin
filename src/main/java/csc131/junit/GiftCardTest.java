ccf8f886769b630d805dd1111cdabb34301f2a5a
Jarod Castillo <Jarodc2017@gmail.com>
2021-02-24 23:49:38 -0800

    added more tests

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -1,8 +1,11 @@
 package csc131.junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
 public class GiftCardTest
 {
  @Test
@@ -44,8 +47,62 @@ public class GiftCardTest
 	 card = new GiftCard(issuingStore, balance);
 	 
 	 whatever = "Remaining Balance: " + String.format("%6.2f", 95.00);
-	 System.out.println(whatever);
 	 assertEquals("deduct()", whatever, card.deduct(5.0));
+ }
+ 
+ @Test
+ public void deduct_AmountDue()
+ {
+	 double balance;
+	 GiftCard card;
+	 int issuingStore;
+	 String whatever;
+
+	 issuingStore = 1337;
+	 balance = 10.00;
+	 card = new GiftCard(issuingStore, balance);
+	 
+	 whatever = "Amount Due: " + String.format("%6.2f", 10.00);
+	 assertEquals("deduct 20.0 from 10.0", whatever, card.deduct(20.0));
+ }
+ 
+ @Test
+ public void deduct_InvalidTransaction()
+ {
+	 double balance;
+	 GiftCard card;
+	 int issuingStore;
+	 String whatever;
+
+	 issuingStore = 1337;
+	 balance = 100.00;
+	 card = new GiftCard(issuingStore, balance);
+	 
+	 whatever = "Invalid Transaction";
+	 assertEquals("deduct negative from positive",
+			 whatever, card.deduct(-10.00));
+ }
+ 
+ @Test
+ public void test_assertThrowsGiftCard()
+ {
+	 //new GiftCard(1,-100.0);
+	 assertThrows("Illegal Balance: -100.0", IllegalArgumentException.class, () -> {new GiftCard(1,-100.0);});
+ }
+ 
+ @Test
+ public void constructor_IncorrectID_Low()
+ {
+	 //new GiftCard(-1, 100.0);
+	 assertThrows("Illegal Store ID: -1 ", IllegalArgumentException.class, () -> {new GiftCard(-1, 100.0);});
+ }
+
+ 
+ @Test
+ public void constructor_IncorrectID_High()
+ {
+	 //new GiftCard(999999999, 100.0);
+	 assertThrows("Illegal Store ID: 999999999", IllegalArgumentException.class, () -> {new GiftCard(999999999, 100.0);});
  }
  
 } 
